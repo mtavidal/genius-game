128edbd1b76cd66f937b1f17a3fc9a94bf9ad4b5
mtavidal <119874263+mtavidal@users.noreply.github.com>
2023-04-05 15:16:11 -0300

    adiciona efeito no game over

--- index.js
+++ index.js
@@ -19,29 +19,29 @@ let sequenceToGuess = [...sequence];
 
 
 const flash = btncolor => {
-    return new Promise ((resolve) => {
+    return new Promise((resolve) => {
         btncolor.className += ' active';
         setTimeout(() => {
-            btncolor.className = btncolor.className.replace(' active','');
+            btncolor.className = btncolor.className.replace(' active', '');
             setTimeout(() => {
                 resolve();
-            },500);
+            }, 500);
         }, 1000);
     });
 };
 
 let canClick = false;
 
-const colorClicked = (colorClicked,teste) => {
+const colorClicked = (colorClicked, teste) => {
     teste.className += ' active';
     setTimeout(() => {
-        teste.className = teste.className.replace(' active','');
-    },500);
-    setTimeout(() => {
-        if(!canClick) return;
+        teste.className = teste.className.replace(' active', '');
+    }, 500);
+    setTimeout( async () => {
+        if (!canClick) return;
         const expectedColor = sequenceToGuess.shift();
-        if(expectedColor === colorClicked) {
-            if(sequenceToGuess.length === 0) {
+        if (expectedColor === colorClicked) {
+            if (sequenceToGuess.length === 0) {
                 sequence.push(getRandomBtnColor());
                 sequenceToGuess = [...sequence];
                 startFlashing();
@@ -51,27 +51,46 @@ const colorClicked = (colorClicked,teste) => {
             sequence.push(getRandomBtnColor());
             sequenceToGuess = [...sequence];
             let contagem = document.querySelector("#contagem");
-            let valor =0;
-            contagem.textContent = valor; 
+            let valor = 0;
+            contagem.textContent = valor;
             const gameover = document.querySelector('#gameover');
             gameover.style.display = "flex";
+            for (let index = 0; index < 5; index++) {             
+                await pisca();
+            }
             const restart = document.querySelector('#restart');
             restart.addEventListener("click", startFlashing);
         }
-    },1000);
+    }, 1000);
 };
+
+async function pisca() {
+    setTimeout(() => {
+        topLeft.className += ' active';
+        topRight.className += ' active';
+        bottomLeft.className += ' active';
+        bottomRight.className += ' active';
+    }, 200);
+    setTimeout(() => {
+        topLeft.className = topLeft.className.replace(' active', '');
+        topRight.className = topRight.className.replace(' active', '');
+        bottomLeft.className = bottomLeft.className.replace(' active', '');
+        bottomRight.className = bottomRight.className.replace(' active', '');
+    }, 500);
+    await new Promise(resolve => setTimeout(resolve,500));
+}
 
 const startFlashing = async () => {
     canClick = false;
     let contagem = document.querySelector("#contagem");
-    let valor =0;
+    let valor = 0;
     const gameover = document.querySelector('#gameover');
     gameover.style.display = "none";
     const titStart = document.querySelector('#titStart');
     titStart.textContent = "PLAY!";
     for (const btncolor of sequence) {
         valor++;
-        contagem.textContent = valor; 
+        contagem.textContent = valor;
         await flash(btncolor);
     }
     canClick = true;
